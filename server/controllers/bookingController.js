99b4b402a162a723e804804dadea233985c4327d
ikgoomophuting-del <ikgoomophuting@gmail.com>
2026-03-12 12:52:17 +0200

    Add booking creation and retrieval functions

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,1 +1,30 @@
+const db = require("../database/db")
 
+exports.createBooking = async (req,res)=>{
+
+const {name,service,date} = req.body
+
+try{
+
+await db.query(
+"INSERT INTO bookings(name,service,date) VALUES($1,$2,$3)",
+[name,service,date]
+)
+
+res.json({message:"Booking created"})
+
+}catch(error){
+
+res.status(500).json({error:error.message})
+
+}
+
+}
+
+exports.getBookings = async (req,res)=>{
+
+const bookings = await db.query("SELECT * FROM bookings")
+
+res.json(bookings.rows)
+
+}
