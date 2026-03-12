993b55e42c0780b2813cebbe73b9334ff2353ab6
ikgoomophuting-del <ikgoomophuting@gmail.com>
2026-03-12 12:55:50 +0200

    Add async function to load and display services

--- client/js/services.js
+++ client/js/services.js
@@ -1,1 +1,22 @@
+async function loadServices(){
 
+const services = await getServices()
+
+const container = document.getElementById("services")
+
+services.forEach(service=>{
+
+const div = document.createElement("div")
+
+div.innerHTML = `
+<h3>${service.name}</h3>
+<p>Price: R${service.price}</p>
+`
+
+container.appendChild(div)
+
+})
+
+}
+
+loadServices()
