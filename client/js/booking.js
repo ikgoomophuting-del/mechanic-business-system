4dbe64cf3d9bb549e8febeb27361161a5e17ea5c
ikgoomophuting-del <ikgoomophuting@gmail.com>
2026-03-12 12:56:27 +0200

    Add async booking function to handle service bookings

--- client/js/booking.js
+++ client/js/booking.js
@@ -1,1 +1,11 @@
+async function bookService(){
 
+const name = document.getElementById("name").value
+const service = document.getElementById("service").value
+const date = document.getElementById("date").value
+
+await createBooking({name,service,date})
+
+alert("Booking successful")
+
+}
