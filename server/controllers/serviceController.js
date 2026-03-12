402f0496e3bfa0e0113dafbbe24e0ef2b0c8fa2f
ikgoomophuting-del <ikgoomophuting@gmail.com>
2026-03-12 12:51:13 +0200

    Add getServices function to retrieve services

--- server/controllers/serviceController.js
+++ server/controllers/serviceController.js
@@ -1,1 +1,17 @@
+const db = require("../database/db")
 
+exports.getServices = async (req,res)=>{
+
+try{
+
+const services = await db.query("SELECT * FROM services")
+
+res.json(services.rows)
+
+}catch(error){
+
+res.status(500).json({error:error.message})
+
+}
+
+}
