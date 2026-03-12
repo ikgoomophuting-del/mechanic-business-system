6250064fb9eb361111f1a1158d39d3c8a44f899f
ikgoomophuting-del <ikgoomophuting@gmail.com>
2026-03-12 12:55:06 +0200

    Add API functions for services and bookings

--- client/js/api.js
+++ client/js/api.js
@@ -1,1 +1,31 @@
+const API_URL = "https://your-render-api.onrender.com/api"
 
+async function getServices(){
+
+const res = await fetch(`${API_URL}/services`)
+return res.json()
+
+}
+
+async function createBooking(data){
+
+await fetch(`${API_URL}/bookings`,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify(data)
+
+})
+
+}
+
+async function getBookings(){
+
+const res = await fetch(`${API_URL}/bookings`)
+return res.json()
+
+}
