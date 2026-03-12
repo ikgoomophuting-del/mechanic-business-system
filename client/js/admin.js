631e7d1c1e98be0fa67af56f75ccca1edb122bf7
ikgoomophuting-del <ikgoomophuting@gmail.com>
2026-03-12 12:58:05 +0200

    Add async function to load and display bookings

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,1 +1,21 @@
+async function loadBookings(){
 
+const bookings = await getBookings()
+
+const list = document.getElementById("bookings")
+
+list.innerHTML = ""
+
+bookings.forEach(b=>{
+
+const li = document.createElement("li")
+
+li.innerHTML = `${b.name} - ${b.service} - ${b.date}`
+
+list.appendChild(li)
+
+})
+
+}
+
+loadBookings()
